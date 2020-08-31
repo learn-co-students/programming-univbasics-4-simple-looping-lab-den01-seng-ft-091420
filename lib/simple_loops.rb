2d42b6c7a192004d7852c661b5cd849294821cf1
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-31 02:11:41 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,40 @@
-# Write your methods here
+
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message,num)
+  count = 0
+  while count < num do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(narray)
+  count = 0
+  while count < narray.length do 
+    puts narray[count]
+    count += 1
+   end
+end
+
+
+def return_string_array(array)
+  i = 0
+  narray = []
+    while i < array.length do 
+      m = array[i].to_s
+  narray.push(m)
+  i += 1
+    end
+  narray
+end
+
+
+
+
